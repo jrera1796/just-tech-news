c5a8c8b0d71ef850b51853352f88231002174dea
jrera1796 <86329054+jrera1796@users.noreply.github.com>
2021-12-15 23:49:05 -0800

    Lots of data

---
+++ routes/api/user-routes.js
@@ -0,0 +1,99 @@
+const router = require('express').Router();
+const { User } = require('../../models');
+
+// GET /api/users
+router.get('/', (req, res) => {
+  //Acess our User model and run .findAll() method
+  //SELECT * FROM users;
+  User.findAll({
+    attributes: {exclude: ['password']}
+  })
+    .then(dbUserData => res.json(dbUserData))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+// GET /api/users/1
+router.get('/:id', (req, res) => {
+  //SELECT * FROM users WHERE id =
+  User.findOne({
+    attributes: {exclude: ['password']},
+    where: {
+      id: req.params.id
+    }
+  })
+    .then(dbUserData => {
+      if (!dbUserData) {
+        res.status(404).json({ message: 'No user found with this id' });
+        return;
+      }
+      res.json(dbUserData);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+// POST /api/users
+router.post('/', (req, res) => {
+  //INSERT INTO users
+  //expects {username: '', email: '', password: ''}
+  User.create({
+    username: req.body.username,
+    email: req.body.email,
+    password: req.body.password
+  })
+    .then(dbUserData => res.json(dbUserData))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+// PUT /api/users/1
+router.put('/:id', (req, res) => {
+  // expects {username: '', email: '', password: ''}
+  // if req.body has exact key/value pairs to match the model, you can just use `req.body` instead
+  User.update(req.body, {
+    where: {
+      id: req.params.id
+    }
+  })
+    .then(dbUserData => {
+      if (!dbUserData[0]) {
+        res.status(404).json({ message: 'No user found with this id' });
+        return;
+      }
+      res.json(dbUserData);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+// DELETE /api/users/1
+router.delete('/:id', (req, res) => { 
+  //Delete data
+  User.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+    .then(dbUserData => {
+      if (!dbUserData) {
+        res.status(404).json({ message: 'No user found with this id' });
+        return;
+      }
+      res.json(dbUserData);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+module.exports = router;
