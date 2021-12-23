bcb4174410037e76ae95569ce5d7b7cc7dd0abc2
jrera1796 <86329054+jrera1796@users.noreply.github.com>
2021-12-22 23:02:40 -0800

    Added login verification

--- models/User.js
+++ models/User.js
@@ -2,7 +2,11 @@ const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 const bcrypt = require('bcrypt');
 //create our User model
-class User extends Model { }
+class User extends Model {
+  checkPassword(loginPw) {
+    return bcrypt.compareSync(loginPw, this.password);
+  }
+}
 
 //define table columns and configuration
 User.init(
